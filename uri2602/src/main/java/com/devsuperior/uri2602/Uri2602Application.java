f489f1147662cfd91f816602293b6e3280b4ff7b
Juan Nobert <nobert407@gmail.com>
2022-08-12 17:49:23 -0300

    URI 2602 querry by SQL

--- uri2602/src/main/java/com/devsuperior/uri2602/Uri2602Application.java
+++ uri2602/src/main/java/com/devsuperior/uri2602/Uri2602Application.java
@@ -1,12 +1,34 @@
 package com.devsuperior.uri2602;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Uri2602Application {
+import com.devsuperior.uri2602.dto.CustomerMinDTO;
+import com.devsuperior.uri2602.projections.CustomerMinProjection;
+import com.devsuperior.uri2602.repositories.CustomerRepository;
 
-	public static void main(String[] args) {
+@SpringBootApplication
+public class Uri2602Application implements CommandLineRunner {
+
+	@Autowired
+	private CustomerRepository repository;
+	
+	public static void main(String[] args)  {
 		SpringApplication.run(Uri2602Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		List<CustomerMinProjection> list = repository.searchByStateSql("RS");
+		List<CustomerMinDTO> result = list.stream()
+				.map(x -> new CustomerMinDTO(x)).collect(Collectors.toList());
+		
+		result.forEach(System.out::println);
+		
 	}
 }
