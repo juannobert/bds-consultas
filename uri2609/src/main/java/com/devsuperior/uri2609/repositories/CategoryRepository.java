23e17dce14341e1b95c3e8c7257972afcfdcd85f
Juan Nobert <nobert407@gmail.com>
2022-08-17 17:24:35 -0300

    URI 2609 query by SQL and JPQL

--- uri2609/src/main/java/com/devsuperior/uri2609/repositories/CategoryRepository.java
+++ uri2609/src/main/java/com/devsuperior/uri2609/repositories/CategoryRepository.java
@@ -1,9 +1,27 @@
 package com.devsuperior.uri2609.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import com.devsuperior.uri2609.dto.CategorySumDTO;
 import com.devsuperior.uri2609.entities.Category;
+import com.devsuperior.uri2609.projections.CategorySumProjection;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+	
+	@Query(nativeQuery = true,value = "SELECT c.name,SUM(p.amount) "
+			+ "FROM categories c "
+			+ "INNER JOIN products p ON p.id_categories = c.id "
+			+ "GROUP BY c.name")
+	List<CategorySumProjection> queryBySQL();
+	
+	@Query(value = "SELECT new com.devsuperior.uri2609.dto.CategorySumDTO(obj.category.name,SUM(obj.amount)) "
+			+ "FROM Product obj "
+			+ "GROUP BY obj.category.name")
+	List<CategorySumDTO> queryByJPQL();
 
 }
