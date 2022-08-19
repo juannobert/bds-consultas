c70aa9faaaf5fdcaac5fdccf7f0d9959472bd4fd
Juan Nobert <nobert407@gmail.com>
2022-08-19 12:31:03 -0300

    URI 2990 query by SQL and JPQL

--- uri2990/src/main/java/com/devsuperior/uri2990/Uri2990Application.java
+++ uri2990/src/main/java/com/devsuperior/uri2990/Uri2990Application.java
@@ -1,10 +1,14 @@
 package com.devsuperior.uri2990;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.devsuperior.uri2990.dto.EmpregadoDeptDTO;
+import com.devsuperior.uri2990.projections.EmpregadoDeptProjection;
 import com.devsuperior.uri2990.repositories.EmpregadoRepository;
 
 @SpringBootApplication
@@ -20,5 +24,17 @@ public class Uri2990Application implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
+		System.out.println("Query by SQL");
+		List<EmpregadoDeptProjection> resultSQL = repository.searchBySql();
+		resultSQL.stream()
+			.map(x -> new EmpregadoDeptDTO(x))
+			.forEach(System.out::println);;
+		
+		System.out.println("\n\n");
+		
+		System.out.println("Query by JPQL");
+		List<EmpregadoDeptDTO> resultJPQL = repository.searchByJPQL();
+		resultJPQL.stream()
+			.forEach(System.out::println);;
 	}
 }
